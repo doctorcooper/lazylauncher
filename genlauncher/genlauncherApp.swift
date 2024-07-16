ab76d6257182cd467968eb8c45d5568f267be6cd
Dmitry Kupriyanov <kuban1408@gmail.com>
2024-07-16 16:58:33 +0400

    Initial Commit

---
+++ genlauncher/genlauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  genlauncherApp.swift
+//  genlauncher
+//
+//  Created by Dmitry Kupriyanov on 16.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct genlauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
